30508ce5c8e0065e4b9ae0ff58cfa0cf154f2767
Luis Jimenez <luisrrjjcc@gmail.com>
2018-07-03 11:31:25 -0600

    Buttons for moving the robot. Button for bending legs and storing.

--- app/src/main/java/edu/galileo/innovation/hexapod/viewer/MainActivity.kt
+++ app/src/main/java/edu/galileo/innovation/hexapod/viewer/MainActivity.kt
@@ -14,7 +14,6 @@ import com.google.firebase.storage.FirebaseStorage
 import android.widget.Button
 import android.widget.Toast
 
-
 class MainActivity : Activity() {
     private val TAG = "MainActivity"
 
@@ -31,18 +30,53 @@ class MainActivity : Activity() {
         firebaseStorage = FirebaseStorage.getInstance()
 
         val newest = FirebaseDatabase.getInstance().reference.child("newest")
-        val state = FirebaseDatabase.getInstance().reference.child("state")
+        val cameraState = FirebaseDatabase.getInstance().reference.child("camera")
+        val moveState = FirebaseDatabase.getInstance().reference.child("movement")
+
+        /*
+        val annotationsListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val keys = dataSnapshot.value
+                Log.d(TAG, "=====> " + keys + "<=====")
+
+                /*
+                    TO DO
+                    THIS GETS THE ANNOTATIONS, BUT SEVERAL SECONDS LATER
+                    SAVE THE LATEST ANNOTATIONS SOMEWHERE ELSE AND FETCH THAT DATA
+                 */
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+            }
+        }
+        */
 
         val imagesListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
+                /* Image */
                 val imageUrl = dataSnapshot.child("url").value.toString()
                 val imageRef = firebaseStorage.getReferenceFromUrl(imageUrl)
 
-                Log.w(TAG, imageUrl)
-
+                Log.d(TAG, imageUrl)
                 GlideApp.with(applicationContext)
                         .load(imageRef)
                         .into(imageView)
+
+                /* Annotations */
+                /*
+                val name = dataSnapshot.child("name").value.toString()
+                val annotations = FirebaseDatabase.getInstance().reference.child("logs").child(name).child("annotations")
+
+                annotations.addListenerForSingleValueEvent(annotationsListener)
+
+                Log.i(TAG, "Annotations:")
+                Log.i(TAG, annotations.toString())
+                /*
+                    TO DO
+                    THIS DOESN'T GET THE ANNOTATIONS, JUST THE URL
+                    TO GET THE ANNOTATIONS, THE LISTENER IS USED
+                 */
+                */
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
@@ -54,18 +88,54 @@ class MainActivity : Activity() {
         val buttonStart = findViewById<Button>(R.id.buttonStart)
         val buttonStop = findViewById<Button>(R.id.buttonStop)
 
+        val buttonForward = findViewById<Button>(R.id.buttonForward)
+        val buttonLeft = findViewById<Button>(R.id.buttonLeft)
+        val buttonRight = findViewById<Button>(R.id.buttonRight)
+        val buttonStand = findViewById<Button>(R.id.buttonStand)
+        val buttonStore = findViewById<Button>(R.id.buttonStore)
+
         buttonStart.setOnClickListener{
             val update = HashMap<String,Any>()
             update["state"] = "started"
-            state.updateChildren(update)
+            cameraState.updateChildren(update)
             Toast.makeText(this@MainActivity, "Taking photos!", Toast.LENGTH_SHORT).show()
         }
 
         buttonStop.setOnClickListener {
             val update = HashMap<String,Any>()
             update["state"] = "stopped"
-            state.updateChildren(update)
+            cameraState.updateChildren(update)
             Toast.makeText(this@MainActivity, "Not taking photos anymore.", Toast.LENGTH_SHORT).show()
+        }
+
+        buttonForward.setOnClickListener{
+            val update = HashMap<String,Any>()
+            update["state"] = "forward"
+            moveState.updateChildren(update)
+        }
+
+        buttonLeft.setOnClickListener{
+            val update = HashMap<String,Any>()
+            update["state"] = "left"
+            moveState.updateChildren(update)
+        }
+
+        buttonRight.setOnClickListener{
+            val update = HashMap<String,Any>()
+            update["state"] = "right"
+            moveState.updateChildren(update)
+        }
+
+        buttonStand.setOnClickListener{
+            val update = HashMap<String,Any>()
+            update["state"] = "stand"
+            moveState.updateChildren(update)
+        }
+
+        buttonStore.setOnClickListener{
+            val update = HashMap<String,Any>()
+            update["state"] = "store"
+            moveState.updateChildren(update)
         }
     }
 
